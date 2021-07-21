a0119214e033595ea6c8f6285d768cfef12bb2c6
Glowrare <aajibolanle@yahoo.com>
2021-07-21 21:19:26 +0100

    update user interface | add clearAllTask function to button | update UX for check and uncheck state of item

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ function personalizer() {
   } else {
     /* html */
     display.innerHTML = `
-    <div class="card mb-4">
+    <div class="card mb-4 rounded-0 shadow-sm">
       <div class="card-body">
         <div class="mb-3">
           <label class="form-label">What is your name?</label>
-          <input type="text" class="form-control" name="username" />
+          <input type="text" class="form-control rounded-0" name="username" />
         </div>
-        <button type="submit" class="btn btn-primary" onclick="storeName()">Save</button>
+        <button type="submit" class="btn btn-outline-primary rounded-0" onclick="storeName()">Save</button>
       </div>
     </div>
     `;
@@ -38,8 +38,9 @@ function personalizer() {
   }
 }
 
+//Fetch input and store Username to local storage for personalizer
 function storeName() {
-  let username = document.querySelector('[name="username"]').value;
+  const username = document.querySelector('[name="username"]').value;
   localStorage.setItem("username", username);
   personalizer();
 }
@@ -53,25 +54,27 @@ const todoForm = document.getElementById("todo-form");
 
 todoForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const todoForm = document.getElementById("todo-form");
   const task = document.querySelector('[name="task"]').value.trim();
-  const priority = document.querySelector('[name="priority"]').value;
-  const done = false;
+  // const priority = document.querySelector('[name="priority"]').value;
+  const priority = document.querySelector(
+    'input[name="priority"]:checked'
+  ).value;
+  // const done = false;
+
   if (task === "") {
-    // alert("Task input cannot empty.");
     document.querySelector('[name="task"]').classList.add("border-danger");
-    document
-      .querySelector("#todo-form .invalid-feedback")
-      .classList.add("d-block");
+    todoForm.querySelector(".invalid-feedback").classList.add("d-block");
   } else {
     document.querySelector('[name="task"]').classList.remove("border-danger");
     todoForm.querySelector(".invalid-feedback").classList.remove("d-block");
+
     const taskObject = {
       task: task,
       priority: priority,
-      status: done,
+      done: false,
       id: Date.now(),
     };
+
     todolist.push(taskObject);
     localStorage.setItem("todoDB", JSON.stringify(todolist));
 
@@ -79,14 +82,12 @@ todoForm.addEventListener("submit", (event) => {
     displayer(todolist);
 
     todoForm.reset();
-
-    // document.getElementById("addNewTask").modal("hide");
   }
 });
 
 // Display tasks
 function displayer(array, existing = false) {
-  let taskDisplay = document.getElementById("task-display");
+  const taskDisplay = document.getElementById("task-display");
   const node = document.createElement("li");
   node.setAttribute("class", "mb-3 col");
 
@@ -95,24 +96,43 @@ function displayer(array, existing = false) {
     element = array[array.length - 1];
   }
 
+  //Set priority color text
+  let priorityIndicator = "";
+
+  if (element.priority === "high") {
+    priorityIndicator = "danger";
+  } else if (element.priority === "medium") {
+    priorityIndicator = "warning";
+  } else {
+    priorityIndicator = "success";
+  }
+
+  //Set list format based on last checked state
+  let checkedState;
+
+  if (element.done) {
+    node.classList.add("text-decoration-line-through", "text-muted");
+    checkedState = "checked";
+  }
+
   /* html */
   node.innerHTML = `
-  <div class="list-item-content bg-info bg-gradient shadow p-3" id="${element.id}">
+  <div class="list-item-content shadow-sm p-3" id="${element.id}">
   <div class="row">
     <div class="col-1">
-      <input class="form-check-input" type="checkbox" id="cb${element.id}" onchange="taskStateChecker(event)" />
+      <input class="form-check-input" type="checkbox" id="cb${element.id}" onchange="taskStateChecker(event)" ${checkedState} />
     </div>
     <div class="col-11">
       <div class="row">
-        <div class="col-10">
+        <div class="col-10 col-sm-11">
           <div class="row">
             <div class="col-12 col-md-8">
               <span class="fw-bold text-uppercase">${element.task}</span>
             </div>
-            <div class="col-12 col-md-4"><span class="fst-italic"> Priority is ${element.priority}</span></div>
+            <div class="col-12 col-md-4"><span class="fst-italic text-${priorityIndicator}"> Priority is ${element.priority}</span></div>
           </div>
         </div>
-        <div class="col-2"><button onclick="itemDeleter(event)" class="btn-close"></button></div>
+        <div class="col-2 col-sm-1"><button onclick="itemDeleter(event)" class="btn-close"></button></div>
       </div>
     </div>
   </div>
@@ -124,7 +144,7 @@ function displayer(array, existing = false) {
 
 // Delete tasks
 function itemDeleter(e) {
-  let confirmation = confirm("Are you sure?");
+  const confirmation = confirm("Are you sure?");
   if (confirmation) {
     const parentEl = e.target.closest(".list-item-content");
     let parentID = parentEl.getAttribute("id");
@@ -137,17 +157,68 @@ function itemDeleter(e) {
 
 // Apply style to list depending on checkbox state
 function taskStateChecker(box) {
+  const taskID = box.target.closest(".list-item-content").getAttribute("id");
+  const taskIndex = todolist.findIndex((item) => item.id == taskID);
+
+  const parentWrapper = document.getElementById("task-display");
+  const parentLi = box.target.closest("li");
+
   if (box.target.checked) {
-    box.target
-      .closest("li")
-      .classList.add("text-decoration-line-through", "text-white");
-    box.target.closest(".list-item-content").classList.add("bg-secondary");
-    box.target.closest(".list-item-content").classList.remove("bg-info");
+    parentLi.classList.add("text-decoration-line-through", "text-muted");
+
+    todolist[taskIndex].done = true;
+
+    //move item to end of list in DOM
+    parentWrapper.appendChild(parentLi);
+
+    //move item to end of stored array
+    const targetEl = todolist[taskIndex]; //store checked List Item object in variable
+
+    todolist = todolist.filter((list) => list.id !== parseInt(taskID)); //filter out current checked list item
+
+    todolist.unshift(targetEl); //Re-add current checked List item to array at index 0
   } else {
-    box.target
-      .closest("li")
-      .classList.remove("text-decoration-line-through", "text-white");
-    box.target.closest(".list-item-content").classList.remove("bg-secondary");
-    box.target.closest(".list-item-content").classList.add("bg-info");
+    parentLi.classList.remove("text-decoration-line-through", "text-muted");
+
+    todolist[taskIndex].done = false;
+
+    //If inbetween other checked items, move current uchecked item just above all checked list item
+    const firstCheckedItem = document.querySelector(
+      ".text-decoration-line-through.text-muted"
+    );
+
+    if (firstCheckedItem) {
+      parentWrapper.insertBefore(parentLi, firstCheckedItem);
+    }
+
+    //Check and move unchecked item out from true done status in stored array
+    const statusArray = todolist.map((item) => item.done); //generate array of item ids
+
+    const lastChecked = statusArray.lastIndexOf(true); //get the last index of item where done status is true
+
+    if (lastChecked >= 0) {
+      const targetEl = todolist[taskIndex]; //store unchecked List Item object in variable
+      todolist = todolist.filter((list) => list.id !== parseInt(taskID)); //filter out current unchecked list item
+
+      todolist.splice(lastChecked, 0, targetEl); //add current unchecked item right after last checked list item
+    }
+  }
+  localStorage.setItem("todoDB", JSON.stringify(todolist));
+}
+
+function deleteAllTasks(confirmed) {
+  if (confirmed) {
+    todolist = [];
+    localStorage.setItem("todoDB", JSON.stringify(todolist));
+
+    //Remove all task from the DOM
+    document.querySelectorAll(".list-item-content").forEach((el) => {
+      el.closest("li").remove();
+    });
   }
 }
+
+document.getElementById("clearAllTasks").addEventListener("click", () => {
+  const confirmation = confirm("Are you sure?");
+  deleteAllTasks(confirmation);
+});
